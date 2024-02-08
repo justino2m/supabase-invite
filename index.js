0929e05df658730bf50bb5a395a84d74c47f994d
justino2m <justin.viola@o2m.systems>
2024-02-08 09:18:26 -0500

    add log files

--- index.js
+++ index.js
@@ -18,6 +18,47 @@ const supabase = createClient(supabase_url, service_role_key, {
 // Define the path to your CSV file
 const csvFilePath = 'users.csv'; // Update with the correct path
 
+// Define an array to store user emails
+const usersToInvite = [];
+
+// Create a unique error log filename with timestamp
+const timestamp = new Date().toISOString().replace(/:/g, '-').replace(/\..+/, '');
+const errorLogFileName = `error_${timestamp}.log`;
+
+// Create error log file if not exists
+fs.writeFileSync(errorLogFileName, '');
+
+async function inviteUsers() {
+  let successfulInvites = 0;
+  let failedInvites = 0;
+  try {
+    for (const userEmail of usersToInvite) {
+      if (!userEmail.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+        fs.appendFileSync(errorLogFileName, `${new Date().toISOString()}: Invalid email: ${userEmail}\n`);
+        console.log(`Invalid email: ${userEmail}`);
+        failedInvites++;
+        continue;
+      }
+
+      const { data, error } = await supabase.auth.admin.inviteUserByEmail(userEmail)
+      if (error) {
+        fs.appendFileSync(errorLogFileName, `${new Date().toISOString()}: Error inviting ${userEmail}: ${error.message}\n`);
+        console.error(`Error inviting ${userEmail}:`, error.message);
+        failedInvites++;
+      } else {
+        successfulInvites++;
+        console.log(`Invited ${userEmail} successfully!`);
+      }
+    }
+  } catch (error) {
+    fs.appendFileSync(errorLogFileName, `${new Date().toISOString()}: Bulk invite error: ${error.message}\n`);
+    console.error('Bulk invite error:', error.message);
+  }
+  console.log('********************************************')
+  console.log(`Invited ${successfulInvites} users successfully.`);
+  console.log(`Encountered errors for ${failedInvites} users. Check ${errorLogFileName} for details.`);
+}
+
 // Read the CSV file and extract user emails
 fs.createReadStream(csvFilePath)
   .pipe(csv())
@@ -29,23 +70,3 @@ fs.createReadStream(csvFilePath)
   .on('end', () => {
     inviteUsers();
   });
-
-// Define an array to store user emails
-const usersToInvite = [];
-
-async function inviteUsers() {
-  try {
-    for (const userEmail of usersToInvite) {
-      const { data, error } = await supabase.auth.admin.inviteUserByEmail(userEmail)
-      if (error) {
-        console.error(`Error inviting ${userEmail}:`, error.message);
-      } else {
-        console.log(`Invited ${userEmail} successfully!`);
-      }
-    }
-  } catch (error) {
-    console.error('Bulk invite error:', error.message);
-  }
-}
-
-// inviteUsers();
